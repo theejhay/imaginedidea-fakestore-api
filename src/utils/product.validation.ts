9deabd77e28623941413c11c98a987e5a1ed38c0
Charisa98 <omole.opeyemi2017@gmail.com>
2026-03-28 23:31:56 +1200

    Utils removed to avoid repetition

--- src/utils/product.validation.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const createCommentSchema = Joi.object({
-    articleId: Joi.string().required(),
-    userId: Joi.string().required(),
-    text: Joi.string().min(1).max(500).required()
-});
